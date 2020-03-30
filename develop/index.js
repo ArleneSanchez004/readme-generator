2a07eb0840311bc79e81298f4b08b59cc6ed5125
Arlene Sanchez <arlene.sanchez004@gmail.com>
2020-03-30 13:57:50 -0400

    added inquire prompts

--- develop/index.js
+++ develop/index.js
@@ -1,5 +1,25 @@
-const questions = [
+var inquirer = require("inquirer");
+var fs = require("fs");
 
+const questions = [
+    inquirer.prompt([
+        {
+            name: "name",
+            type: "input",
+            message: "Enter your name."
+        },
+        {
+            name: "gitName",
+            type: "input",
+            message: "Enter your GitHub username."
+        },
+        {
+            name: "email",
+            type: "input",
+            message: "Enter your email."
+        },
+
+    ])//.then
 ];
 
 function writeToFile(fileName, data) {
